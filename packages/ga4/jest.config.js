616d12e5c1cf627d14d6ff1df7963de9393bc775
James Hill <jahill@groupon.com>
2022-05-23 12:58:50 +0100

    fix: removed getArguments spread operator

--- packages/ga4/jest.config.js
+++ packages/ga4/jest.config.js
@@ -14,10 +14,10 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/', '(.*).d.ts'],
   coverageThreshold: {
     global: {
-      statements: 72,
-      branches: 67,
-      functions: 81,
-      lines: 71,
+      statements: 97,
+      branches: 75,
+      functions: 100,
+      lines: 96,
     },
   },
   transform: {
