d0b7bed4cf2dd83f88177414f017f4d2992861de
James Hill <jahill@groupon.com>
2022-05-24 14:51:00 +0100

    feat: added query param dictionary for heap

---
+++ packages/heap/src/model.ts
@@ -0,0 +1,43 @@
+/* -----------------------------------
+ *
+ * Params
+ *
+ * -------------------------------- */
+
+const params = {
+  appId: 'a',
+  targetClass: 'c',
+  domain: 'd',
+  searchKeyword: 'e',
+  hash: 'g',
+  path: 'h',
+  targetId: 'i',
+  custom: 'k',
+  targetTag: 'n',
+  query: 'q',
+  referrer: 'r',
+  title: 't',
+  targetText: 'x',
+  hierachy: 'y',
+  timeStamp: 'ts',
+  version: 'tv',
+  userId: 'u',
+  sessionId: 's',
+  sentTime: 'st',
+  previousPage: 'pr',
+  utm: {
+    source: 'us',
+    medium: 'um',
+    term: 'ut',
+    campaign: 'ua',
+    content: 'uc',
+  },
+};
+
+/* -----------------------------------
+ *
+ * Export
+ *
+ * -------------------------------- */
+
+export { params };
