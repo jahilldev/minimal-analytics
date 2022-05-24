64e06cc6f1caf60126b02ab878405ea62e32789c
James Hill <jahill@groupon.com>
2022-05-24 14:51:00 +0100

    feat: added query param dictionary for heap

--- packages/heap/src/index.ts
+++ packages/heap/src/index.ts
@@ -1,4 +1,5 @@
 import { getDocument, getClientId, getSessionId } from '@minimal-analytics/shared';
+import { params } from './model';
 
 /* -----------------------------------
  *
@@ -66,31 +67,16 @@ function getQueryParams(trackingId: string, { type, event, debug, error }: IProp
   const { hostname, referrer, title } = getDocument();
 
   const payload = {
-    a: trackingId,
+    [params.appId]: trackingId,
     b: 'web',
-    d: hostname,
-    tv: '4.0',
-    u: getClientId(),
-    r: referrer,
-    s: getSessionId(),
-    t: title,
-    ts: `${Date.now()}`,
-    /*
-    _p: getSessionId('_gapid'),
-    ul: (navigator.language || '').toLowerCase() || void 0,
-    cid: getClientId(),
-    _fv: firstVisit,
-    _s: '1',
-    sid: getSessionId(),
-    sct: getSessionCount(),
-    seg: '1',
-    _ss: sessionStart,
-    _dbg: debug ? '1' : void 0,
-    exd: error?.message || void 0,
-    ...getEventMeta({ type, event }),
-    ...getDocumentMeta(),
-    ...getDeviceMeta(),
-    */
+    [params.domain]: hostname,
+    [params.version]: '4.0',
+    [params.userId]: getClientId(),
+    [params.referrer]: referrer,
+    [params.sessionId]: getSessionId(),
+    [params.title]: title,
+    [params.timeStamp]: `${Date.now()}`,
+    [params.sentTime]: `${Date.now()}`,
   };
 
   Object.keys(payload).forEach((key) => payload[key] ?? delete payload[key]);
