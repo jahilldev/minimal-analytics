940b83a81be589be3c5fe308f8070ddc4e95c487
James Hill <jahill@groupon.com>
2022-05-27 15:48:55 +0100

    test: added more cases for shared package

---
+++ packages/shared/jest.config.js
@@ -0,0 +1,27 @@
+/* -----------------------------------
+ *
+ * Jest
+ *
+ * -------------------------------- */
+
+module.exports = {
+  testEnvironment: 'jsdom',
+  globals: { __DEV__: true },
+  roots: ['<rootDir>'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageDirectory: '<rootDir>/tests/coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '(.*).d.ts'],
+  coverageThreshold: {
+    global: {
+      statements: 56,
+      branches: 50,
+      functions: 40,
+      lines: 50,
+    },
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\js$': 'babel-jest',
+  },
+};
--- packages/shared/tests/utility.test.ts
+++ packages/shared/tests/utility.test.ts
@@ -1,13 +1,65 @@
+import { getRandomId, getHash } from '../src/utility';
+
+/* -----------------------------------
+ *
+ * Variables
+ *
+ * -------------------------------- */
+
+const hashSeed = `${Math.random()}`;
+
 /* -----------------------------------
  *
  * Shared
  *
  * -------------------------------- */
 
-describe('shared', () => {
-  describe('utility', () => {
-    it('needs tests', () => {
-      expect(true).toBe(true);
+describe('shared -> utility', () => {
+  describe('getRandomId', () => {
+    it('generates a unique ID after every call', () => {
+      const result = getRandomId();
+
+      expect(result).not.toEqual(getRandomId());
+      expect(result).not.toEqual(getRandomId());
+      expect(result).not.toEqual(getRandomId());
+    });
+
+    it('generates an ID of correct length', () => {
+      const lengths = [8, 12, 16];
+
+      lengths.forEach((length) => {
+        expect(getRandomId(length).length).toEqual(length);
+      });
+    });
+
+    it('limits character length to 16 digits', () => {
+      const lengths = [32, 64, 128];
+
+      lengths.forEach((length) => {
+        expect(getRandomId(length).length).toEqual(16);
+      });
+    });
+  });
+
+  describe('getHash', () => {
+    it('generates a hash string from an input seed', () => {
+      const iterations = new Array(5);
+
+      const result = getHash(hashSeed);
+
+      expect(result).not.toEqual(hashSeed);
+
+      iterations.forEach(() => {
+        expect(result).toEqual(getHash(hashSeed));
+      });
+    });
+
+    it('generates a hash of correct length', () => {
+      const lengths = [8, 16, 32, 64];
+
+      lengths.forEach((length) => {
+        expect(getHash(hashSeed, length).length).toEqual(length);
+      });
     });
   });
 });
